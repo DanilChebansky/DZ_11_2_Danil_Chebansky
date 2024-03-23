c34bf8ff7cb2a24933719c2e862436a0e13c4170
Danil Chebansky <dan14n97@yandex.ru>
2024-03-24 01:58:10 +0500

    add docstring and new function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_func(value):
+    """Makes string's every symbol upper"""
     return value.upper()
+
+
+def title_func(value):
+    """Makes words' first symbol upper"""
+    return value.title()
